518662706e412d3fe6e9021a5b8f4816096b10f4
ayush <ayushkumar2122.ak@gmail.com>
2026-06-14 17:03:46 +0530

    Init Server
    Signed-off-by: ayush <ayushkumar2122.ak@gmail.com>

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello via Bun!");
+import express from "express"
+import multer from "multer"
+import fs from "fs"
+import pdfParse from "pdf-parse"
+
+const uploads = multer({
+    dest:"uploads/" // make this directory if it does not exist
+})
+const app = express();
+app.use(express.json());
+
+
+app.post("/uploads",uploads.single("pdf"),async (req,res) => {
+    const dataBUffer = fs.readFileSync(req.file!.path)
+    const pdfData = await pdfParse(dataBUffer);
+    const text = pdfData.text
+    if(text.length == 0){
+        res.status(400).json({
+            Error:"Empty pdf or pdf could not be parsed"
+        })
+    }
+
+    const chunks = text.split("\n\n");
+    res.status(200).json({
+        totalChunks : chunks.length,
+        chunks
+    })
+
+})
+
+app.listen(3001,() =>{
+    console.log("🚀 Server Running on port 3001")
+})
